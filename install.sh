252e1ddb8931ca26682df60b0baaa51a826ad24c
deviantony <anthony.lapenna@portainer.io>
2025-05-30 04:07:34 +0000

    chore(install): update KUBESOLO_VERSION to v0.1.3-beta

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ echo "🔍 Detected init system: $INIT_SYSTEM"
 echo "🔍 Detected environment: $ENVIRONMENT"
 
 # Default configuration from environment variables
-KUBESOLO_VERSION="${KUBESOLO_VERSION:-v0.1.2-beta}"
+KUBESOLO_VERSION="${KUBESOLO_VERSION:-v0.1.3-beta}"
 CONFIG_PATH="${KUBESOLO_PATH:-/var/lib/kubesolo}"
 PORTAINER_EDGE_ID="${KUBESOLO_PORTAINER_EDGE_ID:-}"
 PORTAINER_EDGE_KEY="${KUBESOLO_PORTAINER_EDGE_KEY:-}"
