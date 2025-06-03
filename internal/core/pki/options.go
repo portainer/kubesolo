f9ce5eef8c235f58f2c1d3cfc417a7e50f38273f
Steven Kang <skan070@gmail.com>
2025-06-03 17:07:59 +1200

    feat: add extra sans support for the api server (#32)

--- internal/core/pki/options.go
+++ internal/core/pki/options.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"net"
 	"path/filepath"
+	"strings"
 
 	"github.com/portainer/kubesolo/internal/runtime/network"
 	"github.com/portainer/kubesolo/internal/system"
 	"github.com/portainer/kubesolo/types"
+	"github.com/rs/zerolog/log"
 )
 
 // defaultCertOptions returns default options for the specified certificate type
@@ -59,11 +61,11 @@ func defaultCertOptions(certType CertificateType, embedded types.Embedded) CertO
 			"kubernetes.default.svc.cluster.local",
 			"localhost",
 		}
-		opts.IPAddresses = []net.IP{
-			net.ParseIP("127.0.0.1"),
-			net.ParseIP(types.DefaultKubernetesServiceIP),
-		}
+		opts.IPAddresses = []net.IP{net.ParseIP(types.DefaultKubernetesServiceIP)}
 		opts.IPAddresses = append(opts.IPAddresses, ipAddresses...)
+		if len(embedded.APIServerExtraSANs) > 0 {
+			addExtraSANs(&opts, embedded.APIServerExtraSANs)
+		}
 		opts.SignerCertDir = embedded.CACerts.Cert
 		opts.SignerKeyDir = embedded.CACerts.Key
 		opts.CertDir = filepath.Join(embedded.PKIAPIServerDir, "apiserver.crt")
@@ -99,4 +101,27 @@ func defaultCertOptions(certType CertificateType, embedded types.Embedded) CertO
 	}
 
 	return opts
+}
+
+// addExtraSANs adds extra SANs to the certificate options
+// it validates the SANs and adds them to the certificate options
+// if the SAN is a valid IPv4 address, it adds it to the IPAddresses field
+// if the SAN is a valid DNS name, it adds it to the DNSNames field
+// if the SAN is invalid, it logs a warning and skips it
+func addExtraSANs(opts *CertOptions, extraSANs []string) {
+	for _, san := range extraSANs {
+		san = strings.TrimSpace(san)
+		if san == "" {
+			continue
+		}
+
+		if network.IsIPv4Address(san) {
+			opts.IPAddresses = append(opts.IPAddresses, net.ParseIP(san))
+		} else if network.IsDNSName(san) {
+			opts.DNSNames = append(opts.DNSNames, san)
+		} else {
+			log.Warn().Str("component", "pki").Msgf("invalid SAN: %s", san)
+			continue
+		}
+	}
 }
