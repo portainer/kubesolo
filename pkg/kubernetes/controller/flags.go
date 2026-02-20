7be0e2a570dc5f4867e82d2e7ec31a6c39688518
stevensbkang <skan070@gmail.com>
2026-02-20 14:33:37 +1300

    additional plugins

--- pkg/kubernetes/controller/flags.go
+++ pkg/kubernetes/controller/flags.go
@@ -27,7 +27,7 @@ func (s *service) configureControllerManagerFlags(command *cobra.Command) {
 	_ = flags.Set("use-service-account-credentials", "true")
 
 	// controllers
-	_ = flags.Set("controllers", "deployment,replicaset,service,serviceaccount,namespace,attachdetach,endpoint,daemonset,statefulset,root-ca-certificate-publisher-controller,serviceaccount-token-controller,node-ipam-controller,endpointslice-controller,persistentvolume-binder-controller,job-controller,cronjob-controller,garbage-collector-controller")
+	_ = flags.Set("controllers", "deployment,replicaset,service,serviceaccount,namespace,attachdetach,endpoint,daemonset,statefulset,root-ca-certificate-publisher-controller,serviceaccount-token-controller,node-ipam-controller,endpointslice-controller,persistentvolume-binder-controller,job-controller,cronjob-controller,garbage-collector-controller,disruption,csrsigning,clusterrole-aggregation")
 
 	// thresholds
 	_ = flags.Set("terminated-pod-gc-threshold", "0")
