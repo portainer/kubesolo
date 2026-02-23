1c447df21a49512c5478a910c8d1fe742b2b137d
stevensbkang <skan070@gmail.com>
2026-02-24 10:55:28 +1300

    rever back the agent image

--- types/const.go
+++ types/const.go
@@ -27,7 +27,7 @@ const (
 	DefaultKineSocket                = "kine.sock"
 	DefaultControllerManagerDir      = "controller-manager"
 	DefaultSandboxImage              = "portainer/pause:latest"
-	DefaultPortainerAgentImage       = "portainerci/agent:develop"
+	DefaultPortainerAgentImage       = "portainer/agent:2.33.4"
 	DefaultCoreDNSImage              = "coredns/coredns:1.13.1"
 	DefaultLocalPathProvisionerImage = "rancher/local-path-provisioner:v0.0.32"
 	DefaultLocalPathStorageDir       = "local-path-storage"
