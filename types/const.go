f1c0520099491e3af066a6e08febae79e38b766a
Yajith Dayarathna <yajith.dayarathna@portainer.io>
2026-03-25 15:50:09 +1300

    adding back default containerd config. directory

--- types/const.go
+++ types/const.go
@@ -13,6 +13,7 @@ const (
 	DefaultContainerdSocket          = "containerd.sock"
 	DefaultSystemContainerdSock      = "/run/containerd/containerd.sock"
 	DefaultStandardCNIConfDir        = "/etc/cni/net.d"
+	DefaultContainerdConfigDir       = "/etc/containerd/config.d"
 	DefaultContainerdRegistryDir     = "registry"
 	DefaultCNIConfigName             = "10-bridge.conflist"
 	DefaultK8sNamespace              = "k8s.io"
